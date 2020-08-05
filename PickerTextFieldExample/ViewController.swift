6e288581640c9ac73ed2f9085e1c0afa414c3010
Amrit Sidhu <amritsidhu88@gmail.com>
2020-08-05 18:05:28 +0530

    Initial commit

---
+++ PickerTextFieldExample/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  PickerTextFieldExample
+//
+//  Created by Peter Witham on 1/24/17.
+//  Copyright © 2017 Peter Witham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var theTextfield: UITextField!
+    
+    let myPickerData = [String](arrayLiteral: "Peter", "Jane", "Paul", "Mary", "Kevin", "Lucy")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let thePicker = UIPickerView()
+        thePicker.delegate = self
+        theTextfield.inputView = thePicker
+        
+        theTextfield.addDoneOnKeyboardWithTarget(self, action: #selector(self.btnDoneTap(_:)))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func btnDoneTap(_ sender: UIButton) {
+        theTextfield.resignFirstResponder()
+    }
+    
+    // MARK: UIPickerView Delegation
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return myPickerData.count
+    }
+
+    func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return myPickerData[row]
+    }
+
+    func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        theTextfield.text = myPickerData[row]
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+
+}
+
---
+++ Pods/Target Support Files/Pods-PickerTextFieldExample/Pods-PickerTextFieldExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PickerTextFieldExample : NSObject
+@end
+@implementation PodsDummy_Pods_PickerTextFieldExample
+@end
